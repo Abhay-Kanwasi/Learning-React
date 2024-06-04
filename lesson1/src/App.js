f8f72992e2eab981462911ce010662b48febb374
AbhayKanwasi <abhkan24@vzure.com>
2024-06-04 10:56:02 +0530

    project created

---
+++ lesson1/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import Something from "./student";
+
+class App extends Component {
+    render() {
+        return(
+            <>
+            <h1>Hello</h1>
+            <h2>sdfd</h2>
+            <Something name="Abhay"/>
+            <Something name="Ajay"/>
+            <Something name="Sonam"/>
+            </>
+        ) 
+    }
+}
+
+export default App;
---
+++ lesson1/src/student.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+
+const Something = (props) => {
+    return (
+        <>
+            <h1>something is {props.name}</h1>
+        </>
+    )
+}
+
+
+export default Something;
