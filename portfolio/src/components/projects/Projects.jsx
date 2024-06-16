873b1684c02e0238f25e7f20f1d3aaf3f071ceb2
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2024-06-16 12:45:03 +0530

    updated

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -50,46 +50,46 @@ const technologies = "Python";
 
 const Projects = ({ projects }) => {
     const url = "";
-const projectImage = "https://i.pinimg.com/564x/90/3b/bb/903bbb31b5ae50326ae7d2eac38c9a33.jpg";
-const projectTitle = "wow";
-const description = "wow ka desc";
-const technologies = "Python";
-  return (
-    <div className="projects">
-      <Typography variant="h3">
-        Projects <AiOutlineProject />
-      </Typography>
-     
-      <div className="projectsWrapper">
-      <a href={url} className="projectCard" target="black">
-        <div>
-          <img src={projectImage} alt="Project" />
-          <Typography variant="h5">{projectTitle}</Typography>
+    const projectImage = "https://i.pinimg.com/564x/90/3b/bb/903bbb31b5ae50326ae7d2eac38c9a33.jpg";
+    const projectTitle = "wow";
+    const description = "wow ka desc";
+    const technologies = "Python";
+    return (
+      <div className="projects">
+        <Typography variant="h3">
+          Projects <AiOutlineProject />
+        </Typography>
+      
+        <div className="projectsWrapper">
+        <a href={url} className="projectCard" target="black">
+          <div>
+            <img src={projectImage} alt="Project" />
+            <Typography variant="h5">{projectTitle}</Typography>
+          </div>
+          <div>
+            <Typography variant="h4"> About Project</Typography>
+            <Typography>{description}</Typography>
+            <Typography variant="h6">Tech Stack: {technologies}</Typography>
+          </div>
+        </a>
+          {/* {projects.map((item) => (
+            <ProjectCard
+              id={item._id}
+              key={item._id}
+              url={item.url}
+              projectImage={item.image.url}
+              projectTitle={item.title}
+              description={item.description}
+              technologies={item.techStack}
+            />
+          ))} */}
         </div>
-        <div>
-          <Typography variant="h4"> About Project</Typography>
-          <Typography>{description}</Typography>
-          <Typography variant="h6">Tech Stack: {technologies}</Typography>
-        </div>
-      </a>
-        {/* {projects.map((item) => (
-          <ProjectCard
-            id={item._id}
-            key={item._id}
-            url={item.url}
-            projectImage={item.image.url}
-            projectTitle={item.title}
-            description={item.description}
-            technologies={item.techStack}
-          />
-        ))} */}
-      </div>
 
-      <Typography variant="h3" style={{ font: "100 1.2rem 'Ubuntu Mono'" }}>
-        All The Projects Shown Above Are Made By Me <FaRegSmileWink />
-      </Typography>
-    </div>
-  );
+        <Typography variant="h3" style={{ font: "100 1.2rem 'Ubuntu Mono'" }}>
+          All The Projects Shown Above Are Made By Me <FaRegSmileWink />
+        </Typography>
+      </div>
+    );
 };
 
 export default Projects;
